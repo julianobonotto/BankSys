1beafabe4793dc0f8ad8fd2603fefcd732d28cd2
Juliano Kling Bonotto <juliano.bonotto@ibm.com>
2024-09-10 19:57:55 -0300

    README

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # BankSys
 Desafio Bank Sys para Bootcamp.
-Simples, sem Classes.
+Simples, Algumas Classes Adicionadas.
+
+- Números de Conta Corrente iniciam em 1000
+- É preciso melhorar tratamento de erros
+- Aplicar POO
--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 import decimal as d
-import datetime as dt
+from datetime import datetime
+
+class PessoaFisica:
+    def __init__(self, cpf, nome, data_nascimento, endereco):
+        self.cpf = cpf
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.endereco = endereco
+
+class ContaCorrente:
+     def __init__(self, numero, cliente):
+        self.saldo = 0
+        self.numero = numero
+        self.agencia = "0001"
+        self.cliente = cliente
+        self.operacoes_financeiras = []
+
+class Cliente():
+     def __init__(self, pessoa_fisica):
+        self.pessoa_fisica = pessoa_fisica
+        self.contas = []
+        self.indice_conta = 0
+
+class Saque_ou_deposito():
+    def __init__(self, operacao, valor):
+        self.data_hora = datetime.now()
+        self.operacao = operacao
+        self.valor = valor
+
 
 def valida_decimal():
     input_retorno = 0
@@ -15,10 +43,52 @@ def valida_decimal():
         pass
     return input_retorno
 
-def adiciona_linha_extrato(extrato, valor, operacao):
-    data_hora = dt.datetime.now()
+def realiza_operacao_financeira(extrato, valor, operacao):
+    data_hora = datetime.now()
     extrato += f"{data_hora}  {operacao}  R${valor:.2f}\n"
     return extrato
+
+def adiciona_linha_extrato(extrato, valor, operacao):
+    data_hora = datetime.now()
+    extrato += f"{data_hora}  {operacao}  R${valor:.2f}\n"
+    return extrato
+
+def busca_cpf_cadastrado(cpf, clientes_cadastrados):
+    return [cliente for cliente in clientes_cadastrados if cliente.cpf == cpf]
+
+def cadastra_cliente(clientes_cadastrados):
+    cpf = input("Digite CPF: ")
+    nome = input("Digite Nome: ")
+    data_nascimento = input("Data Nascimento: ")
+    endereco = input("Endereço: ")
+
+    cliente_filtrado = busca_cpf_cadastrado(cpf, clientes_cadastrados)
+    if cliente_filtrado:
+            print("CPF/cliente já cadastrado")
+    else:
+        clientes_cadastrados.append(PessoaFisica(cpf = cpf, nome = nome, data_nascimento = data_nascimento, endereco = endereco))
+
+    return clientes_cadastrados
+
+def lista_clientes(clientes_cadastrados):
+    for cli in clientes_cadastrados:
+        print(cli.cpf, " ", cli.nome, " ", cli.data_nascimento, " ", cli.endereco)
+
+def cadastra_nova_conta(clientes_cadastrados, proximo_numero_de_conta):
+    nova_conta = 0
+    cpf = input("Digite CPF do Cliente: ")
+    cliente_cadastrado = busca_cpf_cadastrado(cpf, clientes_cadastrados)
+    if cliente_cadastrado:
+        nova_conta = ContaCorrente(proximo_numero_de_conta, cliente_cadastrado)
+        proximo_numero_de_conta = gera_numero_conta_nova(proximo_numero_de_conta)
+    else:
+        print("CPF não cadastrado, impossivel abrir conta")
+
+    return nova_conta, proximo_numero_de_conta
+
+def gera_numero_conta_nova(proximo_numero_de_conta):
+    proximo_numero_de_conta += 1
+    return proximo_numero_de_conta
 
 
 def sys_bank():
@@ -27,9 +97,14 @@ def sys_bank():
     extrato = '===========EXTRATO============\n'
     LIMITE_NUMERO_SAQUE_DIARIO = 3
     LIMITE_VALOR_SAQUE_POR_OPERACAO = 500.00
+    clientes_cadastrados = []
+    contas = []
+    proximo_numero_de_conta = 1000
 
     menu = """
-
+[nc] Novo Cliente
+[lc] Lista Cliente
+[cc] Nova Conta
 [d] Depositar
 [s] Sacar
 [e] Extrato
@@ -39,34 +114,66 @@ def sys_bank():
     while True:
         opcao = input(menu)
         if opcao == "d":
-            deposito = 0
-            print("Depositar")            
-            deposito += valida_decimal()
-            if deposito > 0 :
-                saldo += deposito
-                extrato = adiciona_linha_extrato(extrato, deposito, " -> DEPOSITO.... +")
-            print(f"o slado é = {saldo}")
+            print("Depositar")
+            conta_para_deposito = input("Informe a Conta Corrente: ")
+            #buscar dentro de -> contas
+            for conta in contas:
+                if str(conta.numero) == conta_para_deposito:
+                    valor = 0
+                    valor += valida_decimal()
+                    if valor > 0 :
+                        deposito = Saque_ou_deposito("DEPOSITO", valor)
+                        conta.operacoes_financeiras.append(deposito)
+                        conta.saldo += valor
+                else:
+                    print("conta não cadastrada")            
 
         elif opcao == "s":
             print("Sacar")
-            saque = 0
-            saque += valida_decimal()
-            if (saque > 0 
-                and saldo >= saque 
-                and contador_saque < LIMITE_NUMERO_SAQUE_DIARIO 
-                and saque <= LIMITE_VALOR_SAQUE_POR_OPERACAO):
-                saldo -= saque
-                contador_saque += 1
-                extrato = adiciona_linha_extrato(extrato, saque, " -> SAQUE....... -")
+            conta_para_saque = input("Informe a Conta Corrente: ")
+            #buscar dentro de -> contas
+            for conta in contas:
+                if str(conta.numero) == conta_para_saque:
+                    valor = 0
+                    valor += valida_decimal()
+                    if (valor > 0 
+                        and conta.saldo >= valor 
+                        and contador_saque < LIMITE_NUMERO_SAQUE_DIARIO 
+                        and valor <= LIMITE_VALOR_SAQUE_POR_OPERACAO):
+                        saque = Saque_ou_deposito("SAQUE  -", valor)
+                        conta.operacoes_financeiras.append(saque)
+                        conta.saldo -= valor
+                else:
+                    print("conta não cadastrada")        
+
+        elif opcao == "nc":
+            print("Cadastro de Cliente")
+            clientes_cadastrados = cadastra_cliente(clientes_cadastrados)
+            for cli in clientes_cadastrados:
+                print(cli.nome)
+
+        elif opcao == "lc":
+            print("Lista de Clientes")
+            lista_clientes(clientes_cadastrados)
+
+        elif opcao == "cc":
+            print("Cadastra Nova Conta")
+            nova_conta, proximo_numero_de_conta = cadastra_nova_conta(clientes_cadastrados, proximo_numero_de_conta)
+            if nova_conta == 0:
+                print("Nao cadastrado")
             else:
-                print("Condiçoes para saque não atendidas")
-            print(f"o slado é = {saldo}")
-
-
+                contas.append(nova_conta)
+                print("Nome Cliente = ", nova_conta.cliente[0].nome, " Numero Conta = ", nova_conta.numero," Saldo = ", nova_conta.saldo)
+        
         elif opcao == "e":
-            print_extrato = ""
-            print_extrato += f"{extrato}\n==============================\n Saldo atual  =  R$  {saldo:.2f}\n"
-            print(print_extrato)
+            conta_corrente_informada = input("Digite numero conta para ver extrato: ")
+            for conta in contas:
+                if str(conta.numero) == conta_corrente_informada:
+                    print(f"Extrato da conta de Numero = {conta.numero}\n")   
+                    for operacao_financeira in conta.operacoes_financeiras:
+                        print(operacao_financeira.data_hora, "   ", operacao_financeira.operacao, "   ", operacao_financeira.valor)
+                    print("\n==========================\n SALDO EM CONTA = ", conta.saldo)
+
         elif opcao == "q":
             print("Sair")
             break
